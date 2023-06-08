81035ac3c9444ed586ca82dceb8239fdab9a428d
Tim Ngeno <h34r54y5@gmail.com>
2023-06-08 19:28:32 +0300

    import calculator functions and perform basic operations

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+import sys
+from calculator_1 import add, sub, mul, div
+
+
+if __name__ == "__main__":
+
+    count = len(sys.argv[1:])
+    if count != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    operators = ['+', '-', '*', '/']
+    if sys.argv[2] not in operators:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+
+    if sys.argv[2] == "+":
+        print(f"{a} + {b} =", add(a, b))
+    elif sys.argv[2] == "-":
+        print(f"{a} - {b} =", sub(a, b))
+    elif sys.argv[2] == "*":
+        print(f"{a} * {b} =", mul(a, b))
+    elif sys.argv[2] == "/":
+        print(f"{a} / {b} =", div(a, b))
