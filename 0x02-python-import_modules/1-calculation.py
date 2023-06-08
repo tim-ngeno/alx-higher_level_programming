52b445d6954cac52f4240e95ba1610b2809ff46f
Tim Ngeno <h34r54y5@gmail.com>
2023-06-08 12:02:06 +0300

    import calculator functions and perform some calculations

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+a = 10
+b = 5
+addition = add(a, b)
+subtraction = sub(a, b)
+multiplication = mul(a, b)
+division = div(a, b)
+
+if __name__ == "__main__":
+    print("{} + {} =".format(a, b), addition)
+    print("{} - {} =".format(a, b), subtraction)
+    print("{} * {} =".format(a, b), multiplication)
+    print("{} / {} =".format(a, b), division)
