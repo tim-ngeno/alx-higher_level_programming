db36a5a884ad6195c6d305191b85bf06d5f16c93
Tim Ngeno <h34r54y5@gmail.com>
2023-06-10 12:07:12 +0300

    write a function that adds two tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    if len(tuple_a) < 2:
+        tuple_a = modify_tuple(tuple_a)
+    elif len(tuple_b) < 2:
+        tuple_b = modify_tuple(tuple_b)
+    c = (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    return c
+
+
+def modify_tuple(tup):
+    if len(tup) == 0:
+        a = list(tup)
+        a.extend([0, 0])
+        tup = tuple(a)
+        return tup
+    elif len(tup) == 1:
+        a = list(tup)
+        a.append(0)
+        tup = tuple(a)
+        return tup
