e46b7d4d943e38c5416d2d67cd3cffc57dea1c70
Tim Ngeno <h34r54y5@gmail.com>
2023-06-10 08:57:13 +0300

    use string format to print integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
-        x = '\n'.join([' '.join(['{}'.format(x) for x in row])])
+        x = '\n'.join([' '.join(['{:d}'.format(x) for x in row])])
         print(x)
