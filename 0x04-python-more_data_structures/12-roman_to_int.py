c1a1a7baa034421335860e558800a0a77153a9a0
Tim Ngeno <h34r54y5@gmail.com>
2023-06-14 22:44:10 +0300

    convert roman numerals to an integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     roman_numerals = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
+        "M": 1000,
         "D": 500,
-        "M": 1000
+        "C": 100,
+        "L": 50,
+        "X": 10,
+        "V": 5,
+        "I": 1,
     }
 
     total = 0
+    p = 0
     for char in roman_string:
         for key, value in roman_numerals.items():
             if char == key:
-                total += value
+                if p == 0 or p >= value:
+                    total += value
+                elif p < value:
+                    total += value - (p * 2)
+                p = value
     return total
