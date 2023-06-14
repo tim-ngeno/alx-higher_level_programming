4932db1e9d432281b2fd38d7ead9d931467ca263
Tim Ngeno <h34r54y5@gmail.com>
2023-06-14 16:58:09 +0300

    correct 1-search_replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list is None or len(my_list) == 0:
-        return None
-    ls_copy = my_list[:]
+    print(ls_copy)
     ls_copy = [t if t != search else replace for t in my_list]
     return ls_copy
