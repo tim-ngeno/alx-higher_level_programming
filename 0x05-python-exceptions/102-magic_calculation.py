9dc918827a19457c57f40d14ea553eb1ebac843d
Tim Ngeno <h34r54y5@gmail.com>
2023-06-26 17:13:18 +0300

    python bytecode

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    for i in range(94, 103):
+        try:
+            if i > a:
+                raise Exception("Too far")
+            result = a ** b / i
+        except:
+            result = b + a
+            break
+    return result
