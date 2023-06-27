db9553259896d0d0f6bbd4988bd21ac0980623d9
Tim Ngeno <h34r54y5@gmail.com>
2023-06-27 14:25:37 +0300

    print a square with the character #

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""Create a class that defines the area of a square"""
+
+
+class Square:
+    """defines a square based on 2-square.py
+
+    Attributes:
+        __size (int): dimension of the square
+    """
+
+    def __init__(self, size=0):
+        """Initializes a Square object
+        Args:
+            size (int): dimension of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        defines the getter for the square size
+        Returns:
+            size of the square
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """defines the setter property
+        Args:
+            value (int): size of the square
+        """
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculates the area of a square
+
+        Returns:
+            current square area
+        """
+        return (self.__size * self.__size)
+
+    def my_print(self):
+        """Prints the square  with a '#' character"""
+        if self.__size == 0:
+            print()
+            return
+        else:
+            y = "".join(['#' for i in range(self.__size)])
+            for j in range(self.__size):
+                print(y)
