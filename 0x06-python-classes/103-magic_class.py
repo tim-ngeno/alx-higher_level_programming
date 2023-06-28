3b174d54df899bc62c229c851d460c02c5486bf5
Tim Ngeno <h34r54y5@gmail.com>
2023-06-28 22:37:48 +0300

    python bytecode

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""disassembly of python bytecode"""
+import math
+
+
+class MagicClass:
+    """Defines a magic class that performs operations on a circle"""
+
+    def __init__(self, radius=0):
+        """Initializes the magic class"""
+        if type(radius) is not int or type(radius) is not float:
+            raise TypeError("radius must be a number")
+        self.radius = radius
+
+    def area(self):
+        """calculates the area of a circle"""
+        return (math.pi * (self.radius ** 2))
+
+    def circumference(self):
+        """calculates the circumference of the circle"""
+        return (math.pi * (self.radius * 2))
