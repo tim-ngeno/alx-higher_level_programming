8ad5c1139875e0925ad425ecc5228c8fb195d36d
Tim Ngeno <h34r54y5@gmail.com>
2023-07-03 15:28:46 +0300

    represent the rectangle using '#'

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -56,4 +56,5 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         else:
-            return ("\n").join("#" * self.__width for i in range(self.__height))
+            return "\n".join("#" * self.__width
+                             for i in range(self.__height))
