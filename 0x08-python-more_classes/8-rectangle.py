ee60425272ff611e9e1e86d85dcc08d78fb98d95
Tim Ngeno <h34r54y5@gmail.com>
2023-07-03 16:41:33 +0300

    correction of 8-rectangle

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -78,10 +78,10 @@ class Rectangle:
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """compares two rectangles and returns the biggest one"""
-        if not isinstance(rect_1, Rectangle):
+        if type(rect_1) != Rectangle:
             raise TypeError("rect_1 must be an \
             instance of Rectangle")
-        if not isinstance(rect_2, Rectangle):
+        if type(rect_2) != Rectangle:
             raise TypeError("rect_2 must be an \
             instance of Rectangle")
         if rect_1.area() >= rect_2.area():
