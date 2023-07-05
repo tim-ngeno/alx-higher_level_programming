9dfaaa4523d537f1ee40bc3c2b90cff1548db447
Tim Ngeno <h34r54y5@gmail.com>
2023-07-05 22:29:26 +0300

    say_my name - update

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,6 +1,15 @@
 #!/usr/bin/python3
+"""
+Prints the full name passed as arguments
+"""
+
+
 def say_my_name(first_name, last_name=""):
-    """Returns the first and last name in a string"""
+    """Returns the first and last name in a string
+    Args:
+        first_name (str): first name argument
+        last_name (str): last name
+    """
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
