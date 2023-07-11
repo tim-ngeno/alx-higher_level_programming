99afedd40a2925573e776d19d5b0963c1eeb01d7
Tim Ngeno <h34r54y5@gmail.com>
2023-07-11 19:39:33 +0300

    add arguments to a list and save them in a json file

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""implementation of 7-add_item"""
+
+import json
+from sys import argv
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+# add the program's arguments to a list
+args = []
+for x in range(1, len(argv)):
+    args.append(argv[x])
+
+filename = "add_item.json"
+
+# save the args to a json file
+save_to_json_file(args, filename)
+load_from_json_file(filename)
