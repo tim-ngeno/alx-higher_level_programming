1f9439ea0782521668393fbccf1ac7d2b7ac0cf8
Tim Ngeno <h34r54y5@gmail.com>
2023-07-12 09:07:39 +0300

    use the same list to add all new arguments

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,13 +7,14 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-# add the program's arguments to a list
-args = []
-for x in argv[1:]:
-    args.append(x)
 
 filename = "add_item.json"
+try:
+    json_list = load_from_json_file(filename)
+except FileNotFoundError:
+    json_list = []
 
+for x in argv[1:]:
+    json_list.append(x)
 # save the args to a json file
-save_to_json_file(args, filename)
-load_from_json_file(filename)
+save_to_json_file(json_list, filename)
