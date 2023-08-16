5d92fe2f679dbf8b52ef2b737156d2bd8bbdc808
Tim Ngeno <h34r54y5@gmail.com>
2023-08-16 23:31:34 +0300

    use correct SQL syntax

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,6 +1,6 @@
 -- Count number of genres
 SELECT genres.name AS genre, COUNT(*) AS number_of_shows
-FROM tv_genres as genres
+FROM tv_genres AS genres
 INNER JOIN tv_show_genres
 ON genres.id = tv_show_genres.genre_id
 GROUP BY genres.name
