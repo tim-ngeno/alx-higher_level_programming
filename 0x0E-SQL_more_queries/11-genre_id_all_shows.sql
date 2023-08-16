363819b6ec73a8d9a95d80fadf322cc4f1d9cfb9
Tim Ngeno <h34r54y5@gmail.com>
2023-08-16 23:09:12 +0300

    list shows by genre id

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,6 @@
+-- Lists all shows in the db hbtn_0d_tvshows
+SELECT tv_shows.title AS title, tv_show_genres.genre_id AS genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+ORDER BY title, genre_id;
