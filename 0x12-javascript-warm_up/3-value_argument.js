e36d15523061891a8f6549df2670a5176cc1239b
Tim Ngeno <timngeno424@gmail.com>
2023-09-11 19:17:57 +0300

    print the first argument passed to the script

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 
 const args = process.argv;
 
-if ((args.length - 2) === 0) {
+if (args.length - 2 === 0) {
   console.log('No argument');
 } else {
   console.log(`${args[2]}`);
--- 0x12-javascript-warm_up/3-value_arguments.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-const args = process.argv;
-
-if ((args.length - 2) === 0) {
-  console.log('No argument');
-} else {
-  console.log(`${args[2]}`);
-}
