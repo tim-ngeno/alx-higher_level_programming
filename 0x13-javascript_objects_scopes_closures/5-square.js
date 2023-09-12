a2efef1874728cfd61a13b129ed89f9eec73f9c8
Tim Ngeno <timngeno424@gmail.com>
2023-09-12 21:38:25 +0300

    create a class Square that inherits from Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+
+// Class Rectangle
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      [this.width, this.height] = [w, h];
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      let row = '';
+      for (let j = 0; j < this.width; j++) {
+        row += 'X';
+      }
+      console.log(row);
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+}
+
+// Class Square
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
