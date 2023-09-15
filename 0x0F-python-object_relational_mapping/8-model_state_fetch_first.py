27abefad7c86f3888bd20617c16513c393d8b1be
Tim Ngeno <timngeno424@gmail.com>
2023-09-15 19:27:27 +0300

    query the first State object from the database

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Get the first State object from the database hbtn_0e_6_usa
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+
+    username = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    DB_URL = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        username, password, database
+    )
+
+    engine = create_engine(DB_URL)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    query = session.query(State).order_by(State.id)
+    if query:
+        print("{}: {}".format(query.first().id, query.first().name))
+    else:
+        print("Nothing")
+
+    session.close()
