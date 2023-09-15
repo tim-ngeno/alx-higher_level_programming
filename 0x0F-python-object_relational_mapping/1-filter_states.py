37d9041d0d65e1ee6b1d7b68c3de1f9ee76bc95d
Tim Ngeno <timngeno424@gmail.com>
2023-09-15 10:27:44 +0300

    use a parameterized query

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -22,7 +22,8 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     # Execute the SQL query
-    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    cur.execute(
+        "SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC", ('N%',))
 
     # Fetch all rows from the table
     rows = cur.fetchall()
