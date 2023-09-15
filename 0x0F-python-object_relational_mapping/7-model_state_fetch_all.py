5f20f50694d3f4f8cc25424a54532935e7ef9b3e
Tim Ngeno <timngeno424@gmail.com>
2023-09-15 18:34:38 +0300

    list all states from hbtn_0e_6_usa via SQLAlchemy

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+List all State objects from the database hbtn_0e_6_usa
+"""
+
+from model_state import Base, State
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+
+if __name__ == "__main__":
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+
+    DB_URL = "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        user, password, database
+    )
+
+    engine = create_engine(DB_URL)
+
+    # Create an ORM 'handle' to the database using Session
+    Session = sessionmaker(bind=engine)
+
+    # Instantiate a session
+    session = Session()
+
+    # Query the DB
+    for instance in session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
