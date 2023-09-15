5fa23c2578152d4e304fbb8f59d15135043d9040
Tim Ngeno <timngeno424@gmail.com>
2023-09-15 12:19:05 +0300

    add 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
     cur = db.cursor()
 
     # Execute the SQL query
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states ORDER BY states.id ASC")
 
     # Fetch all rows from the table
     rows = cur.fetchall()
