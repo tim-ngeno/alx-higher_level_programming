e71a93b2857f497f8db924b39f38341711d6e1bb
Tim Ngeno <timngeno424@gmail.com>
2023-09-18 09:48:32 +0300

    modify cities to have non-null values for id

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -22,7 +22,7 @@ class City(Base):
     __tablename__ = "cities"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey("states.id"))
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
 
     # Creates a one-to-many relationship
     state = relationship(
