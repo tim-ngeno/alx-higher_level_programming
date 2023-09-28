0c6fad81b2f07116df89f1410151eb81390718d9
Tim Ngeno <timngeno424@gmail.com>
2023-09-28 23:05:42 +0300

    Catch me if you can!

---
+++ 0x10-python-network_0/102-catch_me.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# make a request to 0.0.0.0:5000/catch_me, the server should respond
+# with a message 'You got me!'
+curl -sL -X PUT -d "user_id=98" -H "Origin: HolbertonSchool" 0.0.0.0:5000/catch_me
