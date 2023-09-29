2f667163ab6656ce039113367c4841e84da0306a
Tim Ngeno <timngeno424@gmail.com>
2023-09-29 16:16:01 +0300

    add if __name__== '__main__'

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,11 +5,11 @@ Using urllib to perform a GET request
 
 import urllib.request
 
-hbtn_url = urllib.request.Request("https://alx-intranet.hbtn.io/status")
+if __name__ == "__main__":
+    hbtn_url = urllib.request.Request("https://alx-intranet.hbtn.io/status")
 
-with urllib.request.urlopen(hbtn_url) as req:
-    response = req.read()
-
-    print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
-        type(response), response, response.decode('UTF-8')
-    ))
+    with urllib.request.urlopen(hbtn_url) as req:
+        response = req.read()
+        print("Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}".format(
+            type(response), response, response.decode('UTF-8')
+        ))
