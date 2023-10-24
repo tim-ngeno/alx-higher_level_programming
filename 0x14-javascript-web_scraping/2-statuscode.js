5b77a13e2be1a1fb201aaeb673644e203e176b2c
Tim Ngeno <timngeno424@gmail.com>
2023-10-24 17:36:12 +0300

    display the status code of a GET request

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+
+if (process.argv.length <= 2) {
+  console.error('Provide a URL address');
+  process.exit(1);
+}
+
+url = process.argv[2];
+
+request(url, (error, response, body) => {
+  console.log(`code: ${response.statusCode}`);
+});
