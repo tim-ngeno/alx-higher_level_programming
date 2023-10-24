ae1ed9ed508adbd22281284ae7207f49663076bc
Tim Ngeno <timngeno424@gmail.com>
2023-10-24 17:27:57 +0300

    correction of 1-writeme.js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,14 +4,14 @@
 const fs = require('fs');
 
 if (process.argv.length <= 3) {
-  console.log('Usage: <program> <file_path> <text>');
+  console.log('Usage: ./1-writeme.js <file_path> <input_text>');
   process.exit(1);
 }
 
-const content = process.argv[3];
 const filePath = process.argv[2];
+const content = process.argv[3];
 
-fs.writeFile(filePath, content, 'utf8', (error) => {
+fs.writeFile(filePath, content, 'utf-8', (error) => {
   if (error) {
     console.error(error);
   } else {
