0cae0fb5026d8fd77aceea646f9973393ce18be4
Tim Ngeno <timngeno424@gmail.com>
2023-11-01 23:41:23 +0300

    update color of header text using querySelector

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,6 @@
+// Updates the text color of the header element to red
+document.addEventListener('DOMContentLoaded', () => {
+  const header = document.querySelector('header');
+
+  header.style.color = '#FF0000';
+});
